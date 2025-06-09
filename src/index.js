87c6e7f2ce0a2a509071a2dd034180e9090dadc0
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-06-09 19:53:37 +0530

    message queue implemented using rabbitMq

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const amqplib = require('amqplib');
+
+async function connectQueue(){
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue("noti-queue");
+        channel.consume("noti-queue", (data) => {
+            console.log(`${Buffer.from(data.content)}`)
+            channel.ack(data);
+        })
+    } catch (error) {
+        console.log(error);
+    }    
+}
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -14,15 +29,17 @@ app.use('/api', apiRoutes);
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 
-    try {
-        const response = await mailsender.sendMail({
-        from: ServerConfig.GMAIL_EMAIL,
-        to: 'uns.singh03@gmail.com', 
-        subject: 'is this service working?',
-        text: 'If you are reading this, then the service is working fine.',
-    });
-    console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
+    // try {
+    //     const response = await mailsender.sendMail({
+    //     from: ServerConfig.GMAIL_EMAIL,
+    //     to: 'uns.singh03@gmail.com', 
+    //     subject: 'is this service working?',
+    //     text: 'If you are reading this, then the service is working fine.',
+    // });
+    // console.log(response);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+
+    await connectQueue();
 });
