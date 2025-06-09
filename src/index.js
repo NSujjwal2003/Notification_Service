d772f67af576ff1ad0494048ff2ea6de99ad3dce
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-06-10 02:19:08 +0530

    made final changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const amqplib = require('amqplib');
-
+const { EmailService } = require('./services');
 async function connectQueue(){
     try {
         const connection = await amqplib.connect('amqp://localhost');
         const channel = await connection.createChannel();
         await channel.assertQueue("noti-queue");
-        channel.consume("noti-queue", (data) => {
-            console.log(`${Buffer.from(data.content)}`)
+        channel.consume("noti-queue", async (data) => {
+            //const object = JSON.parse(`${Buffer.from(data.content)}`);
+            const object = JSON.parse(data.content.toString());
+            //await EmailService.sendEmail("travisbickle218@gmail.com", object.recepientEmail, object.subject, object.text);
+            await EmailService.sendEmail(ServerConfig.GMAIL_EMAIL, object.recepientEmail, object.subject, object.text);
             channel.ack(data);
         })
     } catch (error) {
